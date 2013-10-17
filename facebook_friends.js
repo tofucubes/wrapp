be40cfdad6568e55d1c35a623c0cb157ea7bd076
stephen lin <tofucubesenator@gmail.com>
2013-10-17 12:10:47 -0700

    force cache update of script for development reasons                     check if logged in                     change path for fb app                     get friendslist info

--- facebook_friends.js
+++ facebook_friends.js
@@ -1,26 +1,80 @@
+function getFriends() {
+    FB.api('/me/friends', function(response) {
+        var friends = response.data;
+        if(response.data) {
+            for(var i=0;i<friends.length;i++){
+                console.log(friends[i].name+'\'s id is '+friends[i].id);
+            }
+        } else {
+            //data didn't come through
+        }
 
+    });
+}
     window.fbAsyncInit = function() {
     // init the FB JS SDK
     FB.init({
         appId      : '186292964892271',                        // App ID from the app dashboard
-        channelUrl : '//WWW.tofucubes.tk/wrapp/', // Channel file for x-domain comms
+        channelUrl : '//www.tofucubes.tk/wrapptest/', // Channel file for x-domain comms
         status     : true,                                 // Check Facebook Login status
         xfbml      : true                                  // Look for social plugins on the page
     });
 
-    // Additional initialization code such as adding Event Listeners goes here
-};
+        // Here we subscribe to the auth.authResponseChange JavaScript event. This event is fired
+        // for any authentication related change, such as login, logout or session refresh. This means that
+        // whenever someone who was previously logged out tries to log in again, the correct case below
+        // will be handled.
 
-FB.login(function(response) {
-    if (response.authResponse) {
-        FB.api('/me', function(info) {
-            login(response, info);
+        FB.Event.subscribe('auth.authResponseChange', function(response) {
+            // Here we specify what we do with the response anytime this event occurs.
+            if (response.status === 'connected') {
+                // The response object is returned with a status field that lets the app know the current
+                // login status of the person. In this case, we're handling the situation where they
+                // have logged in to the app.
+                alert('you are in');
+                getFriends();
+
+            } else if (response.status === 'not_authorized') {
+                // In this case, the person is logged into Facebook, but not into the app, so we call
+                // FB.login() to prompt them to do so.
+                // In real-life usage, you wouldn't want to immediately prompt someone to login
+                // like this, for two reasons:
+                // (1) JavaScript created popup windows are blocked by most browsers unless they
+                // result from direct interaction from people using the app (such as a mouse click)
+                // (2) it is a bad experience to be continually prompted to login upon page load.
+                FB.login(function(response) {
+                    if (response.authResponse) {
+                        FB.api('/me', function(info) {
+                            login(response, info);
+                        });
+                        // The person logged into your app
+                    } else {
+                        // The person cancelled the login dialog
+                    }
+                });
+
+            } else {
+                // In this case, the person is not logged into Facebook, so we call the login()
+                // function to prompt them to do so. Note that at this stage there is no indication
+                // of whether they are logged into the app. If they aren't then they'll see the Login
+                // dialog right after they log in to Facebook.
+                // The same caveats as above apply to the FB.login() call here.
+                FB.login(function(response) {
+                    if (response.authResponse) {
+                        FB.api('/me', function(info) {
+                            login(response, info);
+                        });
+                        // The person logged into your app
+                    } else {
+                        // The person cancelled the login dialog
+                    }
+                });
+            }
         });
-    } else {
-        //user cancelled login or did not grant authorization
-        showLoader(false);
-    }
-}, {scope:'email,user_birthday,status_update,publish_stream,user_about_me'});
+    };
+
+    // Additional initialization code such as adding Event Listeners goes here
+
 
 // Load the SDK asynchronously
 (function(d, s, id){
